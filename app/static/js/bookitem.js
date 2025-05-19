6af59572f9772fd55db6c74e0768586a42598851
Jay-- <buckley_jay@yahoo.com>
2025-05-19 19:39:01 -0400

    Refactor index into components (#94)

---
+++ app/static/js/bookitem.js
@@ -0,0 +1,7 @@
+/**
+ * Placeholder for book item component logic.
+ * Future implementations may move DOM creation from bookshelfUI.js here.
+ */
+export function createBookItem() {
+  // TODO: implement book item creation logic
+}
---
+++ app/static/js/bookshelf.js
@@ -0,0 +1,4 @@
+import { renderBookshelf } from './bookshelfUI.js';
+
+export { renderBookshelf };
+
