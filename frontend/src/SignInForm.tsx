87d9b9b20dfaf9700a7a72afb8fe1a1ef7dd9caf
Jay-- <buckley_jay@yahoo.com>
2025-06-01 06:42:45 -0400

    Refactor sign-in buttons

--- frontend/src/SignInForm.tsx
+++ frontend/src/SignInForm.tsx
@@ -69,14 +69,14 @@ export function SignInForm() {
         <span className="mx-4 text-secondary">or</span>
         <hr className="my-4 grow border-gray-200" />
       </div>
-      <button className="auth-button" onClick={() => void signIn("anonymous")}>
-        Sign in anonymously
-      </button>
       <div className="mt-2">
         <button className="auth-button" onClick={() => void signIn("google")}>
           Sign in with Google
         </button>
       </div>
+      <button className="auth-button" onClick={() => void signIn("anonymous")}>
+        Continue without signing in
+      </button>
     </div>
   );
 }
