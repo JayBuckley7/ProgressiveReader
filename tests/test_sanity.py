ec11a162956217ce844a07a4f8682b09606bd461
Jay-- <buckley_jay@yahoo.com>
2025-06-02 03:51:04 -0400

    Add basic test suite (#225)

---
+++ tests/test_sanity.py
@@ -0,0 +1,11 @@
+"""Basic sanity test module for ProgressiveReader."""
+
+import unittest
+
+
+class SanityTest(unittest.TestCase):
+    """Basic sanity checks."""
+
+    def test_true(self):
+        """Verify that True is truthy."""
+        self.assertTrue(True)
