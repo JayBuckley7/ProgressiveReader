0b80a6d0b338e5aae5d78fa40f235201a7c2c492
Jay-- <buckley_jay@yahoo.com>
2025-07-19 22:18:37 -0400

    Remove style elements when stripping scripts (#403)

--- frontend/src/lib/epubProcessor.ts
+++ frontend/src/lib/epubProcessor.ts
@@ -288,6 +288,7 @@ class EpubProcessor {
     /* ---- helper: remove <script> and <link rel="stylesheet"> ---- */
     _stripScriptsAndStyles(doc: Document): void {
         doc.querySelectorAll('script, link[rel="stylesheet"]').forEach(el => el.remove());
+        doc.querySelectorAll('style').forEach(el => el.remove());
     }
 
     /* ---- helper: flatten toc recursively ---- */
